501fca52b5a7b9ec565db63d0cca8919423c3642
Romashka <rroma13579@gmail.com>
2022-09-19 21:03:20 +0200

    eighth commit

--- js/script.js
+++ js/script.js
@@ -6,11 +6,23 @@ var i = 1;
 btns.forEach(btn => {
 
     const menu = btn.querySelector('.menu');
+    const triangle = btn.querySelector('.triangle');
 
     btn.addEventListener('focus', () => {
         menu.style.display = "block";
+        triangle.style.display = "block";
     });
     btn.addEventListener('focusout', () => {
         menu.style.display = "none";
+        triangle.style.display = "none";
+    });
+
+    var menus1 = btn.querySelectorAll('.menu-1');
+
+    menus1.forEach(menu1 => {
+        menu1.addEventListener('click', () => {
+            var menuicon = menu1.querySelector('.menuicon');
+            console.log(menuicon.background);
+        });
     });
 });
