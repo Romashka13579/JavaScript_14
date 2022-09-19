ee6bd54666b77fd8313fc31a53196d7f4e0fd95f
Romashka <rroma13579@gmail.com>
2022-09-19 19:03:43 +0200

    seventh commit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-var btns = document.querySelectorAll(".btn");
-var menus = document.querySelectorAll(".menu");
+var btns = document.querySelectorAll('.btn');
+var menus = document.querySelectorAll('.menu');
 
 var i = 1;
 
 btns.forEach(btn => {
-    btn.addEventListener('focus', (e) => {
-        e.preventDefault();
+
+    const menu = btn.querySelector('.menu');
+
+    btn.addEventListener('focus', () => {
+        menu.style.display = "block";
+    });
+    btn.addEventListener('focusout', () => {
+        menu.style.display = "none";
     });
 });
